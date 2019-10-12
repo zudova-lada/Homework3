d232036169c7f9f6001e2efd3912b434600215e5
Lada Zudova <zudova.lada@mail.ru>
2019-10-12 11:44:45 +0300

    Исправлена ошибка, при которой функция возвращали тип Any, теперь возвращаемое значение Т Добавлена ошибка, отвечающее за взятие из UserDefoults другого тип

--- Homework3.playground/Contents.swift
+++ Homework3.playground/Contents.swift
@@ -1,27 +1,28 @@
 import UIKit
 
 enum myError: Error {
-    case error
+    case errorNil
+    case errorAnotherType
 }
 
 
-/// Универсальный класс? позволяет добавлять элементы в Userdefaults и забирать их.
-/// Проверяет, выдает ли указатель на объект или nil. Выбрасывает ошибку в случае nil
+/// Универсальный класс, позволяет добавлять элементы в Userdefaults и забирать элементы того же типа, в ином случае выбрасывает ошибку
 class MyStorage <T> {
 
     func addElem(elem:T, key:String){
         UserDefaults.standard.set(elem, forKey: key)
     }
     
-    func getElem(with result: String) throws -> Any {
+    func getElem(with result: String) throws -> T{
         let nn = UserDefaults.standard.object(forKey: result)
         switch nn {
-        case nil: throw myError.error
-        default: return nn!
+        case nil: throw myError.errorNil
+        case is T: return nn as! T
+        default: throw myError.errorAnotherType
         }
     }
     
-    func examp(key: String)->Result<Any, Error>{
+    func examp(key: String)->Result<T, Error>{
         do {
                 let exam = try getElem(with: key)
                 return .success(exam)
@@ -31,30 +32,31 @@ class MyStorage <T> {
     }
 
 }
+UserDefaults.standard.dictionaryRepresentation()
 
+// Добавляем переменную типа Double
+print("Double result:")
+let doubleValue = MyStorage<Double>()
+print(doubleValue.examp(key: "четырнадцать с половиной"))
+doubleValue.addElem(elem: 15.5, key: "пятнадцать с половиной")
+print(doubleValue.examp(key: "пятнадцать с половиной"))
+print("\n")
 
 /// Добавляем тип Int
+print("Int result:")
 let intValue = MyStorage<Int>()
-print(intValue.examp(key: "Nothing"))
 intValue.addElem(elem: 15, key: "fifteen")
-var intM  = intValue.examp(key: "fifteen")
+print(intValue.examp(key: "fifteen"))
+print(intValue.examp(key: "пятнадцать с половиной"))
+print(intValue.examp(key: "15"))
+print("\n")
 
 // Добавляем масссив
+print("Array result:")
 let array = ["Hello","World"]
 let myArray = MyStorage<[String]>()
 myArray.addElem(elem: array, key: "array")
 print(myArray.examp(key: "array"))
-print(myArray.examp(key: "array1"))
-
-// Dictionary
-let dic = ["Name": "Paul", "Country":"UK"]
-let mydic = MyStorage<[String : String]>()
-mydic.addElem(elem: dic, key: "mydictionary")
-print(mydic.examp(key: "array1"))
-print(mydic.examp(key: "mydictionary"))
-
-// bool
-let myBool = MyStorage<Bool>()
-myBool.addElem(elem: true, key: "Correct Id")
-print(myBool.examp(key: "array1"))
-print(myBool.examp(key: "Correct Id"))
+print(myArray.examp(key: "пятнадцать с половиной"))
+print(intValue.examp(key: "myArray"))
+print("\n")
